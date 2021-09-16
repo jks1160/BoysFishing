8d6783c4d713028b05d4d583569c86b4d805991b
YoungHwan <jyh3685@gmail.com>
2021-09-16 14:53:03 +0900

    영환 userDAO 수정

--- src/main/java/com/boys/fishing/user/dao/UserDAO.java
+++ src/main/java/com/boys/fishing/user/dao/UserDAO.java
@@ -3,13 +3,11 @@ package com.boys.fishing.user.dao;
 import java.util.ArrayList;
 
 import com.boys.fishing.board.dto.SumsumDTO;
+import com.boys.fishing.user.dto.UserDTO;
 
 public interface UserDAO {
 
 	ArrayList<SumsumDTO> sumsumlist();
-import com.boys.fishing.user.dto.UserDTO;
-
-public interface UserDAO {
 
 	UserDTO myUserInfo(String u_userid);
 
