c29bcf07a70985df18f53b107292583fe3167b2f
junsungHAN <hjs8213073@naver.com>
2021-09-17 11:29:38 +0900

    한준성 : 페이징 처리 완료

--- src/main/java/com/boys/fishing/user/dao/UserDAO.java
+++ src/main/java/com/boys/fishing/user/dao/UserDAO.java
@@ -6,17 +6,19 @@ import com.boys.fishing.board.dto.SumsumDTO;
 import com.boys.fishing.user.dto.UserDTO;
 
 public interface UserDAO {
+	UserDTO myUserInfo(String u_userid);
+	
+	
+	
+	
+
+	
+	
 	ArrayList<SumsumDTO> sumsumlist(int page, String user, int totalPage);
 
+	ArrayList<SumsumDTO> infoList(int page, String user, int totalPage);
 
-	UserDTO myUserInfo(String u_userid);
-
-
-	ArrayList<SumsumDTO> infoList(int start, String user, int end);
-
-
-	ArrayList<SumsumDTO> revList(int start, String user, int end);
-
+	ArrayList<SumsumDTO> revList(int page, String user, int totalPage);
 
 	int totalPage(String user, String code);
 }
