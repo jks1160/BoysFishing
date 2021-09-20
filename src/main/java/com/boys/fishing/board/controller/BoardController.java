901ca19b9db817464379c3bf9105fa27a3a4d331
BongJoSong <kokobor55@naver.com>
2021-09-20 20:11:00 +0900

    송봉조 qna 작업중

--- src/main/java/com/boys/fishing/board/controller/BoardController.java
+++ src/main/java/com/boys/fishing/board/controller/BoardController.java
@@ -1,8 +1,29 @@
 package com.boys.fishing.board.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.boys.fishing.board.service.BoardService;
+import com.boys.fishing.user.service.UserService;
 
 @Controller
 public class BoardController {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired BoardService service;
+	
+	@RequestMapping(value = "/qna", method = RequestMethod.GET)
+	public ModelAndView QNA(Model model, HttpSession session) {
+		logger.info("QNA 요청");
 
+		return service.qna();
+	}
 }
