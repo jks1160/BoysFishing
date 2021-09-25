1c627760c44f44028af8b84e1cdfb4c29af5925d
DESKTOP-67GRDT1\LEE <sunuiid@naver.com>
2021-09-25 16:28:21 +0900

    선우 날씨정보

--- src/main/java/com/boys/fishing/apis/controller/ApisController.java
+++ src/main/java/com/boys/fishing/apis/controller/ApisController.java
@@ -10,13 +10,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.boys.fishing.apis.service.ApisService;
+import com.boys.fishing.user.dto.UserDTO;
 import com.boys.fishing.user.service.UserService;
 
 //test controller
@@ -60,5 +63,18 @@ public class ApisController {
 		logger.info("일주일 날씨 insert 요청");
 		return service.weekendweatherinsert(params);
 		
+	}
+	
+	//날
+		@RequestMapping(value="/calendar", method = RequestMethod.GET)
+		public String calendar() {
+			logger.info("리스트 진입");
+			return "calendar";
+	}
+	
+		@RequestMapping(value="/calendar_call", method = RequestMethod.GET)
+		public @ResponseBody HashMap<String, Object> calendar_call() {
+			HashMap<String, Object> map = service.todayweahterlist();//u_userid
+			return map;
 	}
 }
